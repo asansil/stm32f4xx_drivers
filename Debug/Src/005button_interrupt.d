b0e230bee5fd9b1466fccf914eac3e6d5523558a
asansil <sansilan000@gmail.com>
2025-06-26 11:29:49 +0200

    Delete Debug directory

--- Debug/Src/005button_interrupt.d
+++
@@ -1,7 +0,0 @@
-Src/005button_interrupt.o: ../Src/005button_interrupt.c \
- C:/Users/asans/Documents/Proyecto_H/Micros/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
- C:/Users/asans/Documents/Proyecto_H/Micros/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h \
- C:/Users/asans/Documents/Proyecto_H/Micros/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
-C:/Users/asans/Documents/Proyecto_H/Micros/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
-C:/Users/asans/Documents/Proyecto_H/Micros/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h:
-C:/Users/asans/Documents/Proyecto_H/Micros/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
--- Debug/drivers/Src/stm32f4xx_gpio_driver.d
+++
@@ -1,8 +0,0 @@
-drivers/Src/stm32f4xx_gpio_driver.o: \
- ../drivers/Src/stm32f4xx_gpio_driver.c \
- C:/Users/asans/Documents/Proyecto_H/Micros/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h \
- C:/Users/asans/Documents/Proyecto_H/Micros/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
- C:/Users/asans/Documents/Proyecto_H/Micros/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h
-C:/Users/asans/Documents/Proyecto_H/Micros/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h:
-C:/Users/asans/Documents/Proyecto_H/Micros/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
-C:/Users/asans/Documents/Proyecto_H/Micros/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h:
